31ed8e39074a9235405befe4634818f9ce22fd95
aashnasahejpal <aashnasahejpal@localhost>
2024-09-19 12:48:33 -0500

    Studio 3

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -1,9 +1,21 @@
 package studio3;
 
+import java.util.Scanner;
+
 public class Sieve {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the highest number: ");
+		int highestNumber = in.nextInt();
+		boolean[] sieve = new boolean[highestNumber + 1];
+		for(int i = 2; i < sieve.length; i++ ) {
+			for(int j = 2*i; j < sieve.length; j+=i ) {
+				int k = sieve.indexOf(j);
+			}
+		}
+		
+		
 
 	}
 
